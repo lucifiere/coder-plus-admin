e44bfc405776bb795e7b796936e0702a90642638
XD.Wang <wangxiandui@meituan.com>
2021-02-04 11:41:27 +0800

    init

---
+++ src/main/java/com/lucifiere/coderplusadmin/CoderPlusAdminApplication.java
@@ -0,0 +1,13 @@
+package com.lucifiere.coderplusadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderPlusAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderPlusAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucifiere/coderplusadmin/CoderPlusAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucifiere.coderplusadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderPlusAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
